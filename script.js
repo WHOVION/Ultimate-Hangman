666838dca9a36329bb18c52a455ffe44c5e1524d
WHOVION <eszrajs@gmail.com>
2022-12-02 17:01:46 -0800

    added some functions, arrays, variables

--- script.js
+++ script.js
@@ -1,2 +1,78 @@
 console.log('hello')
 // make it all in a variable
+
+const resetBtn = document.getElementById('reset')
+let wordDisplay = document.getElementsByClassName('word')
+const board = document.getElementsByClassName('keyboard')
+let word = document.getElementsByClassName('word')
+
+let keys = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's', 
+    't', 
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+]
+
+let countries = [
+    'argentina',
+    'netherlands',
+    'usa',
+    'australia',
+    'japan',
+    'croatia',
+    'brazil',
+    'south korea',
+    'france',
+    'poland',
+    'england',
+    'senegal',
+    'morocco',
+    'spain',
+    'portugal',
+    'switzerland',
+]
+
+// random generator function 
+
+function randomCountry() {
+const countryAns = countries[Math.floor(Math.random() * countries.length)];
+//console.log(countryAns)
+}
+//console.log(countryAns)
+
+// keyboard function 
+
+function keyboard() {
+    <button
+}
+
+
+
+
+
+
+
+
+
