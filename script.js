bc5ae4bb9ab8a92073c59af70d3e306438cdc4dc
WHOVION <eszrajs@gmail.com>
2022-12-07 12:25:50 -0800

    trying to disable everything afte win/lives/timer occurs

--- script.js
+++ script.js
@@ -146,8 +146,7 @@ const reset = document.getElementById('reset')
 const keyboard = document.getElementsByClassName('keyboard')
 const container = document.getElementById('cotainer')
 const word = document.getElementById('word')
-const countdown = document.querySelector('#timer')
-let timer;
+const timeDisplay = document.querySelector('#timer')
 let mistakes = 6
 
  
@@ -164,17 +163,17 @@ let mistakes = 6
     //console.log(placeholder)
 
     let remainingLetters = country.length
-const message = document.querySelector('.message')
-word.innerText = placeholder.join(' ')
-let keys = document.querySelectorAll('.keys')
- //console.log(keys.length)
+    const message = document.querySelector('.message')
+    word.innerText = placeholder.join(' ')
+    let keys = document.querySelectorAll('.keys')
+    //console.log(keys.length)
 
 
     for (let i = 0; i < keys.length; i++) {
     //console.log(keys[i].innerText)
         keys[i].addEventListener('click', function(e) {
-            //setInterval
             //console.log(e.target.innerText)
+            timer()
             let matched = false;
             for (let i = 0; i < country.length; i++) {
                 if (e.target.innerText === country[i]) {
@@ -191,28 +190,43 @@ let keys = document.querySelectorAll('.keys')
                 if(mistakes <= 0) {
                     //console.log('loser')
                     message.innerText = 'YOU NEED TO GO BACK TO GEOGRAPHY CLASS'
+                    //disable.keys
                 }
-            }  if (remainingLetters === 0) {
+            } if (remainingLetters === 0) {
                     //container.style = clear
                     message.innerText = 'WINNER WINNER CHICKEN DINNER'
                 }
             })
     }
 
-    (function(){
-        let sec = 20
-        timer = setInterval(() => {
-            countdown.innerHTML = sec;
+
+    function timer() {
+        let sec = 10
+        activeTimer = setInterval(() => {
+            timeDisplay.innerHTML = sec;
             sec--;
             if (sec < 0) {
                 clearInterval(timer)
+                message.innerText = 'YOU NEED TO GO BACK TO GEOGRAPHY CLASS'
             }
         }, 1000)
-    })()
+    }
 
 
+    function stopEv() {
+        stoptimer()
+    }
 
-
+    // (function(){
+    //     let sec = 20
+    //     timer = setInterval(() => {
+    //         countdown.innerHTML = sec;
+    //         sec--;
+    //         if (sec < 0) {
+    //             clearInterval(timer)
+    //         }
+    //     }, 1000)
+    // })()
 
 
     // reset.addEventListener('click', function(e) {
@@ -228,8 +242,8 @@ let keys = document.querySelectorAll('.keys')
 
 // if word is complete, add message to main box
 // lives is done, add message to whole main box
+// disable all buttons after timer/lives/winner occurs
 // link images to mistakes
-// make active timer
 
 // ?
 // reset button
