e85850a627c268db32f72059cc6e30390f604a10
WHOVION <eszrajs@gmail.com>
2022-12-07 18:49:21 -0800

    making resets made swtich work for errors

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ const container = document.getElementById('cotainer')
 const word = document.getElementById('word')
 const timeDisplay = document.querySelector('#timer')
 let mistakes = 6
+let sec = 20
 
  
     let country = countries[Math.floor(Math.random() * countries.length)]
@@ -66,35 +67,36 @@ let mistakes = 6
                 }
             } if (!matched) {
                 mistakes--;
-                document.getElementById('mistakes').innerText = mistakes;
+                document.getElementById('mistakes').innerText = mistakes; 
                     switch (mistakes) {
                     case 5:
                         console.log('mistakes')
-                        document.getElementById('test1').style.display = 'none'
-                        document.getElementById('test2').classList.remove('hidden')
+                        document.getElementById('test0').style.display = 'none'
+                        document.getElementById('test1').classList.remove('hidden')
                       break;
                     //   console.log(mistakes)
                     case 4:
+                    document.getElementById('test0').classList.add('hidden')
+                    document.getElementById('test1').classList.add('hidden')
+                    document.getElementById('test2').classList.remove('hidden')
+                    break;
+                    case 3:
+                    document.getElementById('test2').classList.add('hidden')
                     document.getElementById('test3').classList.remove('hidden')
-                    //     document.querySelector('img1').style.display.none
-                    //     document.querySelector('img2').style.displayRemove.none
-                    //   break;
-                    // case 3:
-                    //     document.querySelector('img2').style.display.none
-                    //     document.querySelector('img3').style.displayRemove.none
-                    //   break;
-                    // case 2:
-                    //     document.querySelector('img3').style.display.none
-                    //     document.querySelector('img4').style.displayRemove.none
-                    //   break;
-                    // case 1:
-                    //     document.querySelector('img4').style.display.none
-                    //     document.querySelector('img5').style.displayRemove.none
-                    //   break;
-                    // case 0:
-                    //     document.querySelector('img5').style.display.none
-                    //     document.querySelector('img6').style.displayRemove.none
-                    //   break;
+                      break;
+                    case 2:
+                    document.getElementById('test3').classList.add('hidden')
+                    document.getElementById('test4').classList.remove('hidden')
+                      break;
+                    case 1:
+                    document.getElementById('test4').classList.add('hidden')
+                    document.getElementById('test5').classList.remove('hidden')
+                      break;
+                    case 0:
+                    
+                    document.getElementById('test5').classList.add('hidden')
+                    document.getElementById('test6').classList.remove('hidden')
+                      break;
                 }
                 // console.log(mistakes)
                 if(mistakes <= 0) {
@@ -105,8 +107,8 @@ let mistakes = 6
             } if (remainingLetters === 0) {
                     //container.style = clear
                     message.innerText = 'WINNER WINNER CHICKEN DINNER'
-                    document.querySelector('.img0').style.display(none)
-                    document.querySelector('.winner').style.displayRemove(none)
+                    document.getElementById('test0').classList.add('hidden')
+                    document.getElementById('winningPic').classList.remove('hidden')
                 }
                 timer()
             })
@@ -114,7 +116,7 @@ let mistakes = 6
 
 
     function timer() {
-        let sec = 10
+        let sec = 20
         activeTimer = setInterval(() => {
             timeDisplay.innerHTML = sec;
             sec--;
@@ -130,53 +132,46 @@ let mistakes = 6
         stoptimer()
     }
 
+    function resetGame() {
+        mistakes = 6
+        sec = 20    
+        document.getElementById('timer').innerText = sec;
+        document.getElementById('mistakes').innerText = mistakes;
+        // document.querySelectorAll('.hidden').classList('hidden')
+        document.getElementById('test6').classList.add('hidden')
+        document.getElementById('test5').classList.add('hidden')
+        document.getElementById('test4').classList.add('hidden')
+        document.getElementById('test3').classList.add('hidden')
+        document.getElementById('test2').classList.add('hidden')
+        document.getElementById('test1').classList.add('hidden')
+        document.getElementById('test0').style.display = 'inline-block'
+        // document.getElementById('keyboard').disabled = true
+        document.getElementById('keyboard').onclick='this.disabled = true'
 
 
-    // reset.addEventListener('click', function(e) {
-    //     keys.default
-    // })
+        // country = countries[Math.floor(Math.random() * countries.length)]
+        // reset all global variables
+        // buttons undisable
+        console.log('clicked')
+    }
 
-    // function resetBtn(e) {
-    //     reset.addEventListener('click', )
-    // }
+    reset.addEventListener('click', function(e) {
+        resetGame() 
+
+       
+    })
+
+   
+    
 
 
 
 
-// if word is complete, add message to main box
-// lives is done, add message to whole main box
-// disable all buttons after timer/lives/winner occurs
-// link images to mistakes
 
 // ?
-// reset button
+// reset button for word and buttons
+//reset all buttons when win or lose
 
-    // switch (function() {
-                //     case 0:
-                //       document.querySelector('img0').style.display.none
-                //       document.querySelector('img1').style.displayRemove.none
-                //       break;
-                //     case 1:
-                //         document.querySelector('img1').style.display.none
-                //         document.querySelector('img2').style.displayRemove.none
-                //       break;
-                //     case 2:
-                //         document.querySelector('img2').style.display.none
-                //         document.querySelector('img3').style.displayRemove.none
-                //       break;
-                //     case 3:
-                //         document.querySelector('img3').style.display.none
-                //         document.querySelector('img4').style.displayRemove.none
-                //       break;
-                //     case 4:
-                //         document.querySelector('img4').style.display.none
-                //         document.querySelector('img5').style.displayRemove.none
-                //       break;
-                //     case 5:
-                //         document.querySelector('img5').style.display.none
-                //         document.querySelector('img6').style.displayRemove.none
-                //       break;
-                // })
 
 
 
